0225941b5e2bb3b574de0d02c982c323e6aa7795
Mateus Coury <mateuscoury@gmail.com>
2021-07-19 14:33:59 -0300

    Instalação das dependências e criação do server

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const bodyParser = require('body-parser')
+const rotasUsu = require('./controllers/users')
+const cors = require("cors")
+
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use('/usuarios', rotasUsu)
+
+app.use(express.static(__dirname + "/public"))
+
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => console.log(`Escutando na porta:${PORT}`))
+
+module.exports = app
